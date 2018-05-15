f1aee6f03b06d7109165aebe9b77743915d1406a
Ilya-Valasiuk <ilya.valasiuk@gmail.com>
2018-05-15 23:31:08 +0300

    api config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,8 @@
-const LOCAL_HOST = `${window.location.hostname}:3000`;
-const LOCAL_URL_PREFIX = `http://${LOCAL_HOST}/api`;
+// const LOCAL_HOST = `${window.location.hostname}:3000`;
+// const LOCAL_URL_PREFIX = `http://${LOCAL_HOST}/api`;
+const LOCAL_HOST = `social-network-api-test.herokuapp.com`;
+const LOCAL_URL_PREFIX = `https://${LOCAL_HOST}/api`;
+
 
 export {
   LOCAL_HOST,
