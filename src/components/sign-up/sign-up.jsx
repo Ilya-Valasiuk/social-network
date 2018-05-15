2de65a56ac75e71271d15d2ff7ca7b0ffccc1759
Ilya-Valasiuk <ilya.valasiuk@gmail.com>
2018-05-15 23:56:21 +0300

    fix url

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { LOCAL_HOST, LOCAL_URL_PREFIX } from '../../config/config';
 
 export function SignUp() {
   return (
     <Container>
       <p className="h3">Авторизироваться через</p>
       <ul>
-        <li><a href="http://localhost:3000/api/auth/vkontakte">Вконтакте</a></li>
+        <li><a href={`${LOCAL_URL_PREFIX}/auth/vkontakte`}>Вконтакте</a></li>
       </ul>
     </Container>
   );
